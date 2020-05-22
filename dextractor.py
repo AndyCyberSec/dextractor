9364dbd1400db2e9a63f5ba9fdcb68189a728bd2
Andrea Bruschi <moonx2006@gmail.com>
2020-05-22 10:46:50 +0200

    Create detractor.py

---
+++ dextractor.py
@@ -0,0 +1,40 @@
+#!/usr/local/bin/python3
+# AndyCyberSec 2020 - https://github.com/AndyCyberSec
+
+
+from zipfile import ZipFile
+import sys
+import os
+
+def banner():
+
+    print("""Usage: dextractor.py file.apk""")
+
+try:
+    apk = sys.argv[1]
+except:
+    banner()
+    sys.exit(0)
+
+dest_path = os.path.dirname(sys.argv[1])
+
+if len(dest_path) == 0:
+    dest_path = "."
+
+dexs = []
+
+with ZipFile(apk, 'r') as zip:
+    for element in zip.infolist():
+        if ".dex" in element.filename:
+            try:
+                zip.extract(element, dest_path)
+                print("[+] Extraction of " + element.filename + " complete!")
+                dexs.append(dest_path + "/" + element.filename)    
+            except:
+                print("[-] Error during extraction of " + element.filename)
+
+print("[+] Now decompiling dex files...")
+
+stream = os.popen("d2j-dex2jar -o " + dest_path  + ",".join(dexs))
+stream.read()
+print("[+] Decompiling complete!")
